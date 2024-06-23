7cd03a2f93d23a0718568c8aa8a7268256eb0dc0
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-23 22:31:12 +0530

    Mongodb-connect

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,44 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 const port = 5002;
 const app = express();
+const mongoUrl = process.env.MONGO_URL;
+
+//connect to mongodb
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((error) => {
+    console.log("Error connecting to mongodb", error);
+  });
 
 //middlewares
 app.use(express.json());
 app.use(cors());
 
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: String,
+//     age: Number,
+//     contact_number: String,
+//   }  
+// );
 
-app.listen({port}, () => {
+// const userModel = mongoose.model("user",userSchema);
+
+// app.get("/users",async(req,res)=>{
+//   const users = await userModel.find();
+//   res.json(users);
+// })
+
+
+
+app.listen({ port }, () => {
   console.log(`Server is running on port ${port}`);
 });
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import axios from "axios";
+
+export default function HomePage() {
+  
+  return <div> Home Page</div>
+  
+}
