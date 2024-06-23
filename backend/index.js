c18aa7b6be8ac55bc6c98d0baec5976b364725f4
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-23 21:52:15 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ import express from "express";
 const port = 5002;
 const app = express();
 
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+
 app.listen({port}, () => {
   console.log(`Server is running on port ${port}`);
 });
