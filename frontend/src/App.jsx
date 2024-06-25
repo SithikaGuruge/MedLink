a2153352f4d3e8c695a9d6346a0a49fd8fcaf90e
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-25 23:36:40 +0530

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
+import ResponsiveAppBar from "./components/Navbar";
+
 
 const App = () => {
   return (
     <div className="bg-black text-3xl flex justify-center items-center text-white">
-      <HomePage />
+      <ResponsiveAppBar />
     </div>
   );
-}
+};
 
 export default App;
