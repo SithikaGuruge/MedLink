1539bd8cfe327bff6939a9fb2a4dfbfdb104d5c2
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-06-28 18:22:56 +0530

    before editing

--- frontend/src/components/Home/scans.jsx
+++ frontend/src/components/Home/scans.jsx
@@ -8,14 +8,14 @@ export default function Scans() {
       <h1 className="my-6 md:text-5xl text-4xl font-bold text-center md:text-start md:ml-20">Scans</h1>
       
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 py-6">
-    <img src={image} alt="Scans" className="rounded-xl size-96 mx-auto lg:size-max" />
+    <img src={image} alt="Scans" className="rounded-xl size-80 sm:size-96 mx-auto lg:size-max" />
     <div className="mx-auto">
         <h1 className="lg:text-3xl font-bold text-center text-2xl lg:text-start">ScanSmart: <br/> Precision Imaging at Your Fingertips</h1>
-        <h1 className="lg:max-w-lg max-w-sm mx-auto md:text-start text-center lg:text-base py-4">Discover a new era of imaging efficiency with ScanSmart by Medi Link. Seamlessly manage and schedule scans, ensuring timely access to essential diagnostics for improved patient outcomes.</h1>
+        <h1 className="lg:max-w-lg max-w-sm mx-2 sm:mx-auto md:text-start text-center lg:text-base py-4">Discover a new era of imaging efficiency with ScanSmart by Medi Link. Seamlessly manage and schedule scans, ensuring timely access to essential diagnostics for improved patient outcomes.</h1>
         <h1 className="bold text-xl text-center font-semibold md:text-start"> MRI</h1>
-        <h1 className="lg:max-w-lg max-w-sm mx-auto md:text-start text-center lg:text-base py-4">Provide precise imaging for diagnosing brain, joint, and organ conditions without radiation exposure.</h1>
+        <h1 className="lg:max-w-lg max-w-sm mx-2 sm:mx-auto md:text-start text-center lg:text-base py-4">Provide precise imaging for diagnosing brain, joint, and organ conditions without radiation exposure.</h1>
         <h1 className="bold text-xl text-center font-semibold md:text-start"> CT Scans</h1>
-        <h1 className="lg:max-w-lg max-w-sm mx-auto md:text-start text-center lg:text-base py-4">Offer rapid, detailed images crucial for diagnosing injuries, tumors, and internal conditions with exceptional clarity.</h1>
+        <h1 className="lg:max-w-lg max-w-sm mx-2 sm:mx-auto md:text-start text-center lg:text-base py-4">Offer rapid, detailed images crucial for diagnosing injuries, tumors, and internal conditions with exceptional clarity.</h1>
     </div>
       </div>
       <div className="flex justify-center">
