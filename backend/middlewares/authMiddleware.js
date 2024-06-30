766fb2aaa8e7c85e3aaa0d1e3b2a563cc5fbe2be
Rusira de Silva <rusira.21@cse.mrt.ac.lk>
2024-06-30 19:36:09 +0530

    change variable name in verifyToken

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -15,7 +15,7 @@ const verifyToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.username = decoded;
+    req.user = decoded;
     next();
   } catch (error) {
     return res.status(401).json({ error: "Invalid or expired token" });
